01830e5412c5778441a74cf6ae5d604eb1572e5d
lelona <3020288C@student.gla.ac.uk>
2025-04-16 16:12:46 +0100

    Remove unused folder:Demo_test

--- Demo_test/build/CMakeFiles/FruitCameraEnglish.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for FruitCameraEnglish.
-# This may be replaced when dependencies are built.
--- Demo_test/build/CMakeFiles/FruitCameraEnglish_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/FruitCameraEnglish_autogen"
-  "FruitCameraEnglish_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/FruitCameraEnglish_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
