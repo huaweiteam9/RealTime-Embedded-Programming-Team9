63239b9a167ede548f79d878854089fb55c7addd
lelona <3020288C@student.gla.ac.uk>
2025-04-16 16:18:20 +0100

    Remove unused folder:Demo_test2

--- Demo_test2/build/CMakeFiles/demo_02_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/demo_02_autogen"
-  "demo_02_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/demo_02_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
